afd11cdf64ed578a428bfab901ce0694bbcdcdba
Ubuntu <ubuntu@ip-172-31-47-213.us-east-2.compute.internal>
2021-06-06 09:29:17 +0000

    vm-commit

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from flask_cors import CORS, cross_origin
 
 import tensorflow as tf
@@ -12,34 +12,40 @@ from keras import backend as K
 
 application = Flask(__name__)
 CORS(application)
+cors = CORS(application, resources = {
+    r"/*": {
+        "origins": "*"
+    }
+})
 
 best_model =  load_model('BalanceNet_T20.h5')
 with open('tokenizer.pickle', 'rb') as handle:
-	tokenizer = pickle.load(handle)
+    tokenizer = pickle.load(handle)
 
 @application.route('/')
-def index():    
+def index():
     return 'Application Works!'
 
 @application.route('/process', methods= ['POST'])
-def process(user_text):
+def process():
+    
     # show the user profile for that user
     firstName = request.form['firstName']
-	
-	MAX_SEQUENCE_LENGTH = 30
-	result = ""
-	text = ["" for _ in range(5)]
-	text[0] = str(firstName)
-	sequences_test = tokenizer.texts_to_sequences(text)
-	data_int_t = pad_sequences(sequences_test, padding='pre', maxlen=(MAX_SEQUENCE_LENGTH-5))
-	data_test = pad_sequences(data_int_t, padding='post', maxlen=(MAX_SEQUENCE_LENGTH))
-	
-	y_prob = best_model.predict(data_test)
-	result = str(y_prob[0][0]) + ',' + str(y_prob[0][1]) +',' + str(y_prob[0][2]) + ',' + str(y_prob[0][3]) + ',' + str(y_prob[0][4])
-	
-	return result
+    	
+    MAX_SEQUENCE_LENGTH = 30
+    result = ""
+    text = ["" for _ in range(5)]
+    text[0] = str(firstName)
+    sequences_test = tokenizer.texts_to_sequences(text)
+    data_int_t = pad_sequences(sequences_test, padding='pre', maxlen=(MAX_SEQUENCE_LENGTH-5))
+    data_test = pad_sequences(data_int_t, padding='post', maxlen=(MAX_SEQUENCE_LENGTH))
+    	
+    y_prob = best_model.predict(data_test)
+    result = str(y_prob[0][0]) + ',' + str(y_prob[0][1]) +',' + str(y_prob[0][2]) + ',' + str(y_prob[0][3]) + ',' + str(y_prob[0][4])
+    	
+    return result
 	
     
 if(__name__=='__main__'):
     from waitress import serve
-    serve(application, host='0.0.0.0, port=8000)
+    serve(application, host='0.0.0.0', port=8080)
