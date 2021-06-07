90e2c0086a544a1e03931cba8bb2867921f92f47
DebparnoB <69355442+DebparnoB@users.noreply.github.com>
2021-06-07 22:19:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,85 +1,51 @@
-import re
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, make_response, request
+from flask_cors import CORS, cross_origin
 
+import tensorflow as tf
 from keras.models import load_model
-# Run this cell to mount your Google Drive.
-#from google.colab import drive
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-import pandas as pd
 import pickle
 import tensorflow as tf
 from keras import backend as K
 
-application = app = Flask(__name__)
+
+app = Flask(__name__)
+CORS(app)
+cors = CORS(app, resources = {
+    r"/*": {
+        "origins": "*"
+    }
+})
+
 best_model =  load_model('BalanceNet_T20.h5')
-graph = tf.get_default_graph()
 with open('tokenizer.pickle', 'rb') as handle:
-        tokenizer = pickle.load(handle)
-"""
-@app.route('/init')
-def init():
-    global best_model
-    # load the pre-trained Keras model
-    best_model = load_model('BalanceNet1.h5')
-    #graph = tf.get_default_graph()
-"""  
+    tokenizer = pickle.load(handle)
 
+@app.route('/')
+def index():
+    return 'Application Works!'
 
-@app.route('/process',methods= ['POST'])
+@app.route('/process', methods= ['POST'])
 def process():
-    result = ""
-    MAX_SEQUENCE_LENGTH = 30
-    firstName = request.form['firstName']
-    #lastName = request.form['lastName']
-    #best_model =  load_model('BalanceNet1.h5')
     
+    # show the user profile for that user
+    firstName = request.form['firstName']
+    	
+    MAX_SEQUENCE_LENGTH = 30
+    result = ""
     text = ["" for _ in range(5)]
     text[0] = str(firstName)
-    
-    
     sequences_test = tokenizer.texts_to_sequences(text)
     data_int_t = pad_sequences(sequences_test, padding='pre', maxlen=(MAX_SEQUENCE_LENGTH-5))
     data_test = pad_sequences(data_int_t, padding='post', maxlen=(MAX_SEQUENCE_LENGTH))
-    #with graph.as_default():
-    global graph
-    with graph.as_default():
-        y_prob = best_model.predict(data_test)
-    lis = list(y_prob[0])
-    emot = ["Neutral", "Happy", "Sad", "Hate","Anger"]
-    maxi = lis.index(max(lis))
-    result = result + '    Neutral: ' + str(y_prob[0][0]) + ' ......Happiness: ' + str(y_prob[0][1]) +' ......Sadness: ' + str(y_prob[0][2]) + ' ......Hatred: ' + str(y_prob[0][3]) + ' ......Anger: ' + str(y_prob[0][4]) + '..................... Prominent Emotion: ' + emot[maxi]
-    
-    #K.clear_session()
-    
-    
-    #output = firstName + lastName
-    if (firstName):
-        return jsonify({'output': result})
-    return jsonify({'error' : 'Missing data!'})
-
-    
-@app.route('/')
-def index():
-    return "App Working..."
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-"""
-@app.route('/', methods = ['POST'])
-def index():
-    MAX_SEQUENCE_LENGTH = 30
-    
-    best_model =  load_model('BalanceNet1.h5')
-    #data2 = pd.read_csv('train.csv')
-    text = request.form['firstName']
-    x = text.split(' ')
-    y = [int(k) for k in x]
-    data_int_t = pad_sequences([y, [], [], [], []], padding='pre', maxlen=(MAX_SEQUENCE_LENGTH-5))
-    data_test = pad_sequences(data_int_t, padding='post', maxlen=(MAX_SEQUENCE_LENGTH))
+    	
     y_prob = best_model.predict(data_test)
-    #processed_text = text.upper()
-    return jsonify({'request' : str(y_prob[0][0])})
-"""
+    result = str(y_prob[0][0]) + ',' + str(y_prob[0][1]) +',' + str(y_prob[0][2]) + ',' + str(y_prob[0][3]) + ',' + str(y_prob[0][4])
+    	
+    return result
+	
+    
+if(__name__=='__main__'):
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=8080)
